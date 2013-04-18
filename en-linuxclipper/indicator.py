65d7fb9ba78826f05aa24d7fc4126c72dc0e44f0
Özcan ESEN <ozcanesen@gmail.com>
2013-04-18 04:37:52 +0300

    add indicator menu items

--- en-linuxclipper/indicator.py
+++ en-linuxclipper/indicator.py
@@ -1,13 +1,42 @@
-
 from gi.repository import Gtk
 from gi.repository import AppIndicator3 as appindicator
 
+from i18n import _
+
 class Indicator:
     def __init__(self):
-        ind = appindicator.Indicator.new("notify", "everpad-mono", appindicator.IndicatorCategory.APPLICATION_STATUS)
-        ind.set_status(appindicator.IndicatorStatus.ACTIVE)
+        self.ind = appindicator.Indicator.new("notify", "everpad-mono", appindicator.IndicatorCategory.APPLICATION_STATUS)
+        self.ind.set_status(appindicator.IndicatorStatus.ACTIVE)
 
         menu = Gtk.Menu()
-        ind.set_menu(menu)
 
-        Gtk.main()
+        open_dashboard = Gtk.MenuItem(_("Open Dashboard"))
+        menu.append(open_dashboard)
+        
+        menu.append(Gtk.SeparatorMenuItem.new())
+
+        capture_screen = Gtk.MenuItem(_("Capture Full Screen"))
+        menu.append(capture_screen)
+
+        capture_window = Gtk.MenuItem(_("Capture Window"))
+        menu.append(capture_window)
+
+        capture_selection = Gtk.MenuItem(_("Capture Selection"))
+        menu.append(capture_selection)
+
+        menu.append(Gtk.SeparatorMenuItem.new())
+
+        preferences = Gtk.MenuItem(_("Preferences"))
+        menu.append(preferences)
+
+        quit = Gtk.MenuItem(_("Quit"))
+        quit.connect('activate', self.quit_callback)
+        menu.append(quit)
+
+        menu.show_all()
+        self.ind.set_menu(menu)
+
+
+    def quit_callback(self, event):
+        Gtk.main_quit()
+        
--- src/clipper.h
+++
@@ -1,5 +0,0 @@
-void capture_screen();
-
-void capture_window();
-
-void capture_area();
