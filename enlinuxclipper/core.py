aad1c1190e9fa4670f000750cdb4e4aa033dd0ba
Özcan ESEN <ozcanesen@gmail.com>
2013-05-17 00:07:17 +0300

    Added License disclaimer

--- enlinuxclipper/core.py
+++ enlinuxclipper/core.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python
+# -*- coding: utf-8; -*-
+"""
+Copyright (C) 2013 - Özcan ESEN <ozcanesen@gmail.com>
+
+This file is part of EN-LinuxClipper.
+
+EN-LinuxClipper is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+EN-LinuxClipper is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+"""
 
 from gi.repository import Gtk
 
